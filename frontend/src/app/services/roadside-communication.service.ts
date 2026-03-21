a466ddff77f25cfe1c9044fac508b40ca3765bf2
TechnoCraftCons <44950527+TechnoCraftCons@users.noreply.github.com>
2026-03-20 20:14:06 -0500

    fix(roadside-communication): update apiUrl to remove redundant '/api' segment

--- frontend/src/app/services/roadside-communication.service.ts
+++ frontend/src/app/services/roadside-communication.service.ts
@@ -11,7 +11,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class RoadsideCommunicationService {
-  private apiUrl = `${environment.apiUrl}/api/roadside`;
+  private apiUrl = `${environment.apiUrl}/roadside`;
 
   constructor(private http: HttpClient) {}
 
